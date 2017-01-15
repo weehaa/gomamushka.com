623e8c4fc758eccfdbfdc5b138480e92a6cd863b
weehaa <vzg1000@mail.ru>
2017-01-15 12:33:34 +0400

    news and footer fix

---
+++ footer.php
@@ -0,0 +1,46 @@
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  ga('create', 'UA-69126635-1', 'auto');
+  ga('send', 'pageview');
+</script>
+
+<div class="container-fluid bg1">
+  <div class="row">
+    <div class="container">
+  <div class="row">
+    <div class="col-md-12 col-sm-12 text-left">
+        <div class="fb-like" data-href="https://www.facebook.com/s.v.mamushka/" data-width="320" data-layout="standard" data-action="like" data-show-faces="false" data-share="true"></div>
+    </div>
+  </div>
+    <!-- <div class="container-fluid header1"> -->
+        <div class="row">
+            <div class="nobg col-lg-6 col-md-6 col-sm-5 col-xs-12 left">
+              <div class="row">
+                <div class="container-fluid">
+                  <div class="row">
+                    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 text-left">
+                      <img src="<?php bloginfo('template_directory'); ?>/images/svmamushka_logo1.png" alt="s/v MAMUSHKA" />
+                    </div>
+                    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 text-right">
+                        <h4>&#169; 2015-<?php echo date('Y') ?></h4>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="nobg col-lg-6 col-md-6 col-sm-7 col-xs-12  email">
+              <h4>&#128386;&nbsp;<a href="mailto:mamushka@gomamushka.com" class="white">mamushka@GoMamushka.com</a>
+              </h4>
+            </div>
+        </div>
+    <!-- </div> -->
+</div>
+</div>
+</div>
+		</div> <!-- end of wrapper -->
+	</body>
+</html>
--- front-page.php
+++ front-page.php
@@ -7,8 +7,11 @@ if (is_front_page()) :
 <div class="container-fluid bg1">
     <div class="container header1">
         <div class="row">
-            <div class="nobg col-lg-2 col-md-3 col-sm-3 col-xs-6 right text-right"><a href="<?php bloginfo('url'); ?>"><img src="<?php bloginfo('template_directory'); ?>/images/svmamushka_logo1.png" alt="Путешествие на яхте Мамушка" /></a></div>
-            <div class="nobg col-lg-10 col-md-9 col-sm-9 col-xs-6"><?php echo do_shortcode ('[shortmenu menu="Menu 1"  enhance="true"  display="inline" menu_anchor_color="#ffffff"  menu_anchor_hover_color="#ffffff"  submenu_anchor_color="#e0e0e0"  submenu_anchor_hover_color="#ffffff"  submenu_transparency="0.8"  arrow="false"  is_responsive="true" responsive="750"]')?></div>
+            <div class="nobg col-lg-2 col-md-3 col-sm-3 col-xs-6 right text-right"><a href="<?php bloginfo('url'); ?>">
+              <img src="<?php bloginfo('template_directory'); ?>/images/svmamushka_logo1.png" alt="Путешествие на яхте Мамушка" /></a>
+            </div>
+            <div class="nobg col-lg-10 col-md-9 col-sm-9 col-xs-6"><?php echo do_shortcode ('[shortmenu menu="Menu 1"  enhance="true"  display="inline" menu_anchor_color="#ffffff"  menu_anchor_hover_color="#ffffff"  submenu_anchor_color="#e0e0e0"  submenu_anchor_hover_color="#ffffff"  submenu_transparency="0.8"  arrow="false"  is_responsive="true" responsive="750"]')?>
+            </div>
         </div>
     </div>
 </div>
@@ -43,67 +46,76 @@ if (is_front_page()) :
 </div>
 
 <div class="container-fluid">
-   <div class="row">
-   <div class="col-md-12 col-sm-12 wide-header">
-   <?php // if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('wide-header') ) { } ?>
-   <?php  echo do_shortcode('[mapsmarker layer="1"]'); ?>
-   </div>
-   </div>
-</div>
+    <div class="row">
+        <div class="col-md-12 col-sm-12 wide-header">
+          <?php  echo do_shortcode('[mapsmarker layer="1"]'); ?>
 
-<div class="row bg1 text-center">
-   <div class="col-md-12 col-sm-12 text-center storyline">
-     <h1>Путешествие на яхте "Мамушка"</h1>
-     <img src="<?php bloginfo('template_directory'); ?>/images/wheel.png">
-     <p>"Мамушка" - парусная яхта, Hunter 340, на которой в начале лета 2015 года мы, московский офисный планктон, отправились в путешествие по мировому океану. Почему?
-     Потому что жизнь одна, а провести ее всю в офисе, пусть даже занимаясь любимым делом, не хочется.
-     Пришло время сделать паузу и посмотреть - что там за горизонтом?
-    </p>
-  </div>
+        </div>
+     </div>
+   </div>
 </div>
-
-<div class="container-fluid news">
-  <div class="container news">
-    <h2 class = "text-center">Новые путевые заметки</h2>
-    <div class="row" style="margin-bottom: 3em;">
-      <?php $newposts_count = 3;
-      $args = array('showposts' => $newposts_count,'cat' => -4);
-          $my_query = new WP_Query($args);
-              while ($my_query->have_posts()) : $my_query->the_post();
-              $do_not_duplicate = $post->ID; ?>
-            <div class="col-md-4 col-sm-6 col-xs-12 news">
-              <div class="bg3 news-col">
-                <?php if ( has_post_thumbnail() ) {
-                           the_post_thumbnail('thumbnail');
-                              } ?>
-                <h3 class = "news-title">
-                  <a href="<?php the_permalink(); ?>"><?php the_date('d.m.Y'); ?>.
-                     <?php the_title(); ?></a><br/>
-                </h3>
-                <div class="text-left">
-                <?php global $more;
-            		$more = 0;
-                  the_content(); ?>
-                </div>
-             </div>
-           </div>
-            <?php endwhile; ?>
-      </div>
-    </div>
-  </div>
-</div>
+<!--bug in shortcode --></div>
 
 <div class="container-fluid">
-  <div class="row bg-bottom" >
-    <h2 class = "text-center">Фотографии нашего приключения</h2>
+  <div class="row bg1 text-center">
+     <div class="col-md-12 col-sm-12 text-center storyline">
+       <h1>Путешествие на яхте "Мамушка"</h1>
+       <img src="<?php bloginfo('template_directory'); ?>/images/wheel.png">
+       <p>"Мамушка" - парусная яхта, Hunter 340, на которой в начале лета 2015 года мы, московский офисный планктон, отправились в путешествие по мировому океану. Почему?
+       Потому что жизнь одна, а провести ее всю в офисе, пусть даже занимаясь любимым делом, не хочется.
+       Пришло время сделать паузу и посмотреть - что там за горизонтом?
+      </p>
+    </div>
+  </div>
+
+
+  <div class="row">
     <div class="container">
-      <div id="primary-sidebar" class="primary-sidebar widget-area" role="complementary">
-        <?php dynamic_sidebar( 'home_bottom_1' ); ?>
+      <div class = "row">
+        <div class = "col-md-12">
+          <h2 class = "text-center">Новые путевые заметки</h2>
+        </div>
+      </div>
+
+      <div class="row" style="margin-bottom: 3em;">
+        <?php $newposts_count = 3;
+        $args = array('showposts' => $newposts_count,'cat' => -4);
+            $my_query = new WP_Query($args);
+                while ($my_query->have_posts()) : $my_query->the_post();
+                $do_not_duplicate = $post->ID; ?>
+              <div class="col-md-4 col-sm-6 col-xs-12 px-0">
+                <div class="bg3 news-col">
+                  <?php if ( has_post_thumbnail() ) {
+                             the_post_thumbnail('thumbnail');
+                                } ?>
+                  <h3 class = "news-title">
+                    <a href="<?php the_permalink(); ?>"><?php the_date('d.m.Y'); ?>.
+                       <?php the_title(); ?></a><br/>
+                  </h3>
+                  <div class="text-left">
+                  <?php global $more;
+              		$more = 0;
+                    the_content(); ?>
+                  </div>
+               </div>
+             </div>
+              <?php endwhile; ?>
       </div>
     </div>
   </div>
 </div>
-
+<div class="container-fluid">
+    <div class="row bg-bottom" >
+      <div class = "col-md-12">
+        <h2 class = "text-center">Фотографии нашего приключения</h2>
+      </div>
+      <div class="container">
+        <div id="primary-sidebar" class="primary-sidebar widget-area" role="complementary">
+          <?php dynamic_sidebar( 'home_bottom_1' ); ?>
+        </div>
+      </div>
+    </div>
+</div>
 
 <?php
 get_footer();
